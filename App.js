9076d5a624f0321a470184ce7ab8242cb8224291
Chidi <chidiebubeokoye755@gmail.com>
2023-10-19 13:13:33 +0100

    first commit

---
+++ App.js
@@ -0,0 +1,27 @@
+import logo from './logo.svg';
+import './App.css';
+import {Header} from './components/Header'
+import Home from './Pages/Home';
+import LogInform from './components/LogInform';
+import Dassh from './components/DashBoard';
+
+
+const App = ()=>{
+   
+   return (
+    <div className="App">
+      <Home />
+      <Section/>
+      <Button/>
+            
+     
+    </div>
+  );
+
+
+
+}
+
+
+
+export default App;
